847ed448a04755da7ab6ae77f3bff9c329a82061
Onphairin Chandee <finkopr2546@gmail.com>
2026-01-30 11:42:47 +0700

    FFFF

--- project-cafe/app/page.js
+++ project-cafe/app/page.js
@@ -29,7 +29,7 @@ export default function CafeList() {
             <Button color="inherit" sx={{ color: '#FFFFFF' }}>Home Home</Button>
           </a>
           <a href='/cafes'>
-            <Button color="inherit" sx={{ color: '#FFFFFF' }}>Review</Button>
+            <Button color="inherit" sx={{ color: '#9a4646ff' }}>Review</Button>
           </a>
         </Toolbar>
       </AppBar>
